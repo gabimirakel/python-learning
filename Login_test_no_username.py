3314859432308aad879a193702963b734499ed68
gabimirakel <gabrielhakimmaulana10@gmail.com>
2026-07-26 08:23:44 +0700

    Add files via upload
    A collection of Python exercises and mini-projects documenting my progress from beginner to automation scripting.

---
+++ Login_test_no_username.py
@@ -0,0 +1,9 @@
+password = "Ambatukam"
+user_input = input("Enter Password:")
+if user_input == password:
+    print("Access Granted, Dreamybull")
+else:
+    print("Incorrect password, try again. You are fake thugs!")
+
+
+    
---
+++ login_test_with_username.py
@@ -0,0 +1,8 @@
+username = "Dreamybull"
+password = "Ambatukam"
+user_input = input("username: ")
+password_input = input("password: ")
+if user_input == username and password_input == password:
+    print("Access Granted, Dreamybull")
+else:
+    print("Incorrect username or password, try again. You are fake thugs!")
